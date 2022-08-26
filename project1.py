e6555329825778fbba2d5258f8631f1fcdda81f8
Nisha1008 <104624740+Nisha1008@users.noreply.github.com>
2022-08-26 11:55:05 +0530

    Add files via upload
    email verification with python

---
+++ project1.py
@@ -0,0 +1,43 @@
+# email validation
+'''email = input("Enter your Email:")
+k,j,d = 0,0,0
+if len(email) > 6:  #g@g.in
+    if email[0].isalpha():
+        if ("@" in email) and (email.count("@")==1):
+            if (email[-4] == ".") ^ (email[-3] == "."):
+                for i in email:
+                    if i==i.isspace():
+                        k =1
+                    elif i.isalpha():
+                        if i == i.upper():
+                            j =1
+                    elif i.isdigit():
+                        continue
+                    elif i == "_" or i=="." or i=="@":
+                        continue
+                    else:
+                        d =1
+                if k==1 or j==1 or d ==1:
+                    print("wrong email 5")
+                else:
+                    print("Right email")
+
+            else:
+                print("wrong email 4")
+        else:
+            print("wrong email 3") 
+    else:
+        print("wrong email 2")
+else:
+    print("wrong email")'''
+
+
+#using regex
+
+import re
+email_condition = "^[a-z]+[\._]?[a-z 0-9]+ [@]\w+[.]\w{2,3}$"
+user_email = input("Enter your Eamil: ")
+if re.search(email_condition,user_email):
+    print("Right Email")
+else:
+    print("Wrong Email:")
